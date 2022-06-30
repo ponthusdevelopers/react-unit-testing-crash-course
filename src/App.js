8e52071091c03b3fec9040ddba17db65200868aa
MokoMaracucho <desmoulin.federico@gmail.com>
2022-06-30 15:09:25 +0200

    19'45''

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import './App.css'
 import Users from './Components/Users'
 
 function App() {
-  return (
-    <div className='App'>
-        <Users />
-    </div>
-  )
+
+    const users = [
+        {id: 1, name: 'John', age: 15},
+        {id: 2, name: 'Kelly', age: 20}
+    ]
+
+    return (
+        <>
+            {users.map((user, index) => {
+                return (
+                    <Users
+                        key={`users-${index}`}
+                        user={user}
+                    />
+                )
+            })}
+        </>
+    )
 }
 
 export default App
--- src/Components/__tests__/Users.test.js
+++ src/Components/__tests__/Users.test.js
@@ -1,9 +1,27 @@
 import { render, screen, cleanup } from '@testing-library/react'
 import Users from '../Users'
 
-test('Should render Users component', () => {
+/* test('Should render Users component', () => {
     render(<Users />)
     const userElement = screen.getByTestId('user-1')
     expect(userElement).toBeInTheDocument()
     expect(userElement).toHaveTextContent('Hi')
+}) */
+
+test('Should render illegal user', () => {
+    const user = {id: 1, name: 'John', age: 15}
+
+    render(<Users user={user} />)
+    const userElement = screen.getByTestId('user-1')
+    expect(userElement).toBeInTheDocument()
+    expect(userElement).toHaveTextContent('John')
+})
+
+test('Should render legal user', () => {
+    const user = {id: 2, name: 'Kelly', age: 20}
+
+    render(<Users user={user} />)
+    const userElement = screen.getByTestId('user-2')
+    expect(userElement).toBeInTheDocument()
+    expect(userElement).toHaveTextContent('Kelly')
 })
